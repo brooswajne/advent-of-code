1db1e11d42b97126f6c70d7746379078d1f0f919
Willem Garnier <willem.garnier@ipsos.com>
2022-12-09 16:58:48 +0000

    Work out the stacks with some very lazy code

---
+++ 2022/Day05/solution.py
@@ -0,0 +1,42 @@
+import os
+
+input_file_path = os.path.join(
+    os.path.dirname(os.path.realpath(__file__)),
+    "input",
+)
+
+stacks: list[list[str]] = []
+CRATE_SIZE = 3
+STACK_WIDTH = CRATE_SIZE + 1
+
+with open(input_file_path, "r") as input_file:
+    # parse drawing
+    for line in input_file:
+        if line.strip()[0] != "[": break
+        print(line.strip("\n"))
+        for offset in range(0, len(line), STACK_WIDTH):
+            stack = int(offset / STACK_WIDTH)
+            crate = line[offset : offset + CRATE_SIZE]
+            contents = crate[1]
+            if contents == " ": continue
+            while len(stacks) <= stack: stacks.append([])
+            stacks[stack].append(contents)
+
+    for stack in stacks: stack.reverse()
+    print("Initial:")
+    for stack in stacks: print(stack)
+
+    for line in input_file:
+        if not line.startswith("move"): continue
+        _move, amount, _from, source, _to, target = line.split(" ")
+        amount = int(amount)
+        source = int(source) - 1
+        target = int(target) - 1
+        for _ in range(0, amount):
+            crate = stacks[source].pop()
+            stacks[target].append(crate)
+    
+    print("Done:")
+    for stack in stacks: print(stack)
+    answer = "".join([ stack[-1] for stack in stacks ])
+    print("Answer:", answer)
